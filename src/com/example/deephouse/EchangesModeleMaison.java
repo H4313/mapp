374598572e2c3895d60577fa53bd4f18aed7a243
Steevens Segura <steevens.segura@gmail.com>
2014-02-09 21:01:37 +0100

    Avancement sur la communication HTTP

---
+++ src/com/example/deephouse/EchangesModeleMaison.java
@@ -0,0 +1,78 @@
+package com.example.deephouse;
+
+import com.example.deephouse.HttpCommunication;
+import com.h4313.deephouse.housemodel.House;
+import org.json.*;
+
+/**
+ * Created by Steevens on 30/01/14.
+ */
+public class EchangesModeleMaison
+{
+    /**
+     * Informer le modèle de la maison d’une action utilisateur.
+     * @param numPiece
+     * @param objetAction : ce que l'utilisateur à actionné. EX : temperature, humidité...
+     * @param valeurCapteur
+     * @return
+     */
+    public static void actionUtilisateur(int numPiece, int objetAction, float valeurCapteur) throws JSONException
+    {
+        JSONObject j = new JSONObject();
+        
+        j.put("piece", numPiece);
+        j.put("typeAction", objetAction);
+        j.put("valeur", valeurCapteur);
+        
+        try {
+        	HttpCommunication.sendPost("URLURLURL",j.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+    /**
+     * Informer le modèle de la maison à l’ajout d’un capteur.
+     * @param numPiece
+     * @param idCapteur : numero de serie du capteur (saisit par l'utilisateur).
+     * @param typeCapteur : nature du capteur (capteur de presence, de lumière...) identifié par un entier.
+     * @return
+     */
+    public static void ajoutCapteur(int numPiece, int idCapteur, int typeCapteur) throws JSONException
+    {
+        JSONObject j = new JSONObject();
+        
+        j.put("piece", numPiece);
+        j.put("capteur", idCapteur);
+        j.put("type", typeCapteur);
+
+        try {
+        	HttpCommunication.sendPost("URLURLURL",j.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+    /**
+     * reception periodique des valeurs des capteurs depuis le modele de la maison
+     * @param jsonHouse
+     * @throws JSONException
+     */
+    public static void receptionInfosCapteurs() throws JSONException
+    {
+        String jsonResponse;
+        
+        try {
+        	jsonResponse = HttpCommunication.sendGet("URLURLURL");
+            JSONObject j = new JSONObject(jsonResponse);
+        	House h = House.getInstance();
+        	h = (House) j.get("house");
+        	// updateVue(h); // MAJ vue
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+}
+
+
+
---
+++ src/com/example/deephouse/HttpCommunication.java
@@ -0,0 +1,93 @@
+package com.example.deephouse;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
+public class HttpCommunication 
+{
+	// private final String USER_AGENT = "Mozilla/5.0";
+ 
+	public static void main(String[] args) throws Exception
+	{
+		HttpCommunication http = new HttpCommunication();
+ 
+		System.out.println("Testing 1 - Send Http GET request");
+		http.sendGet("");
+ 
+		System.out.println("\nTesting 2 - Send Http POST request");
+		http.sendPost("","");
+ 
+	}
+ 
+	// HTTP GET request
+	public static String sendGet(String url) throws Exception {
+ 
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+ 
+		// optional default is GET
+		con.setRequestMethod("GET");
+ 
+		//add request header
+		//con.setRequestProperty("User-Agent", USER_AGENT);
+ 
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+ 
+		//print result
+		System.out.println(response.toString());
+		
+		return response.toString();
+	}
+ 
+	// HTTP POST request
+	public static void sendPost(String url, String urlParameters) throws Exception {
+ 
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+ 
+		//add request header
+		con.setRequestMethod("POST");
+		//con.setRequestProperty("User-Agent", USER_AGENT);
+		//con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+ 
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+ 
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("Response Code : " + responseCode);
+ 
+		BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+ 
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+ 
+		//print result
+		System.out.println(response.toString());
+ 
+	}
+}
